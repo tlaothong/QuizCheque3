4ad030947c9320c2de57238bbd281e9e93d2c385
Somkiet <somsor@perfenterprise.com>
2018-11-06 17:32:19 +0700

    init project and interface

---
+++ ChequeFinder/ChequeFinder.BizDomain/BizDomain/ChequeFinderBizDomain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChequeFinder.BizDomain
+{
+    public class ChequeFinderBizDomain : IChequeFinderBizDomain
+    {
+        public IEnumerable<int> FindCheque(IEnumerable<int> prices)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> FindFactor(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChequeFinder/ChequeFinder.BizDomain/BizDomain/IChequeFinderBizDomain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChequeFinder.BizDomain
+{
+    public interface IChequeFinderBizDomain
+    {
+        IEnumerable<int> FindCheque(IEnumerable<int> prices);
+        IEnumerable<int> FindFactor(int number);
+    }
+}
