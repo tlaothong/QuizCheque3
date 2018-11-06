d7de43b6e23b87742115e6526c6cc98f7171f640
woraput <woraput@kkumail.com>
2018-11-06 17:42:49 +0700

    set up unit test

---
+++ QuizCheque3/QuizModel.cs
@@ -0,0 +1,16 @@
+namespace QuizCheque3
+{
+    public class QuizModel
+    {
+        public int Amount1 { get; set; }
+        public int Amount2 { get; set; }
+        public int Amount3 { get; set; }
+        public int Amount4 { get; set; }
+
+        public int Check1 { get; set; }
+        public int Check2 { get; set; }
+        public int Check3 { get; set; }
+
+
+    }
+}
