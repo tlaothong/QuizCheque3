86b0fa9e175a5eed13f635399f7cdb3728cf1c0c
Anawat <anawat@perfenterprise.com>
2018-11-06 18:05:43 +0700

    Add write cheque calculator
    Signed-off-by: Anawat <anawat@perfenterprise.com>

---
+++ Cheque/​​​Cheque/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ___Cheque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chequeList = new List<int>();
+            var priceList = new List<int>();
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine($"Enter amount #{i}:");
+                var priceStr = Console.ReadLine();
+                var isInt = int.TryParse(priceStr, out var price);
+                if (!isInt || price == 0) {
+                    Console.WriteLine("Data have error or price is 0, plsease start again.");
+                    Console.ReadKey();
+                    return; }
+                priceList.Add(price);
+            }
+            var ingorePrice = 99;
+            priceList = priceList.OrderBy(it => it).ToList();
+
+            var isIngorePriceIndex0 = priceList[0] == priceList[1];
+            var isIngorePriceIndex2 = priceList[1] == priceList[2]
+                || priceList[0] + priceList[1] == priceList[2];
+            var isIngorePriceIndex3 = priceList[0] + priceList[1] + priceList[2] == priceList[3]
+                || priceList[0] + priceList[1] == priceList[3]
+                || priceList[0] + priceList[2] == priceList[3]
+                || priceList[1] + priceList[2] == priceList[3]
+                || priceList[2] == priceList[3];
+
+            var threeChequePayAble = isIngorePriceIndex0 || isIngorePriceIndex2 || isIngorePriceIndex3;
+            if (!threeChequePayAble)
+            {
+                Console.WriteLine("Can't find 3 cheques for all those amounts.");
+            }
+            else
+            {
+                ingorePrice = isIngorePriceIndex0 ? 0
+                    : isIngorePriceIndex2 ? 2
+                    : 3;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (ingorePrice != i) chequeList.Add(priceList[i]);
+                }
+
+                Console.WriteLine("You should write the following cheques");
+                for (int i = 0; i < chequeList.Count; i++)
+                {
+                    Console.WriteLine($"#{i + 1} ${chequeList[i]}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
