d5528bef445d428bf19a483012eed5c24f25728f
aphaphon <pui.aphaphon@gmail.com>
2018-11-06 19:02:08 +0700

    Cheque
    Signed-off-by: aphaphon <pui.aphaphon@gmail.com>

---
+++ Cheque/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cheque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            System.Console.WriteLine(a);
+        }
+    }
+}
